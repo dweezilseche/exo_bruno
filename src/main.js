7ec0c159c457cf5212fa59d66bdc149fd576b265
dweezilseche <dweezilseche@gmail.com>
2026-04-25 16:24:27 +0200

    Refactor animations and styles: removed unused animation functions and text-split module, updated styles for buttons, and adjusted main.js to streamline initialization.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import { initSmoothScroll } from "./smooth-scroll.js";
-import { initAnimations } from "./animations.js";
-import { initTextSplit } from "./text-split.js";
 import { initSliders } from "./slider.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initSmoothScroll();
-  initTextSplit();
-  initAnimations();
   initSliders();
 });
